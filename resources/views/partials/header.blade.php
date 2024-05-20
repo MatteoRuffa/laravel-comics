58c571af3cf73b715d3ceb1ee78d034cba2866d5
MatteoRuffa <cortese_17@protonmail.com>
2024-05-20 14:56:01 +0200

    added header  and db

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,10 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+<header class="container">
+    <div><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo"></div>
+    <div>
+        <ul>
+            @foreach($dccomics as $dccomic)
+                <li><a href="#">{{ $dccomic['name'] }}</a></li>
+            @endforeach
+        </ul>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $dccomics = config('db_comics.dccomics');
+    return view('home', compact('dccomics'));
 });
